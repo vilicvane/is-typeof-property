6d450102a5d49544d8ec859b8f446f766251e424
vilicvane <vilicvane@live.com>
2023-10-23 03:49:32 +0800

    Add arrayOf

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,22 +1,48 @@
 export default function isTypeOfProperty<
-  T extends object,
-  TKey extends keyof T,
-  TType extends
-    | 'string'
-    | 'number'
-    | 'bigint'
-    | 'boolean'
-    | 'symbol'
-    | 'undefined'
-    | 'object'
-    | 'function',
+  TObject extends object,
+  TKey extends keyof TObject,
+  TType extends GeneralType,
 >(
-  object: T,
+  object: TObject,
   key: TKey,
   type: TType,
-): object is Extract<T, {[_ in TKey]: TypeOfTypeOfString<TType>}> {
-  return typeof object[key] === type;
+): object is Extract<
+  TObject,
+  {
+    [_ in TKey]: TType extends ExtendedType<infer T>
+      ? T
+      : TypeOfTypeOfString<Extract<TType, string>>;
+  }
+> {
+  return isTypeOf(object[key], type);
 }
+
+export function arrayOf<TType extends GeneralType>(
+  type: TType,
+): ExtendedType<TypeOfGeneralType<TType>>;
+export function arrayOf(type: GeneralType): (value: unknown) => boolean {
+  return value => {
+    return Array.isArray(value) && value.every(item => isTypeOf(item, type));
+  };
+}
+
+function isTypeOf(value: unknown, type: GeneralType): boolean {
+  if (typeof type === 'function') {
+    return type(value);
+  } else {
+    return typeof value === type;
+  }
+}
+
+export type TypeOfString =
+  | 'string'
+  | 'number'
+  | 'bigint'
+  | 'boolean'
+  | 'symbol'
+  | 'undefined'
+  | 'object'
+  | 'function';
 
 export type TypeOfTypeOfString<TType extends string> = TType extends 'string'
   ? string
@@ -35,3 +61,12 @@ export type TypeOfTypeOfString<TType extends string> = TType extends 'string'
   : TType extends 'function'
   ? (...args: any[]) => any
   : never;
+
+export type ExtendedType<T> = (value: unknown) => value is T;
+
+export type GeneralType = TypeOfString | ExtendedType<unknown>;
+
+export type TypeOfGeneralType<TType extends GeneralType> =
+  TType extends ExtendedType<infer T>
+    ? T
+    : TypeOfTypeOfString<Extract<TType, string>>;
